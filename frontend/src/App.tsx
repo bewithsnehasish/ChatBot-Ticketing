8a88477771cc0934ae4ae6389d92e2bf1f5548bc
Snehasish Mandal <mandalsneh97@gmail.com>
2024-09-17 19:26:54 +0530

    Final page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import AppLayout from "./layout/AppLayout";
 import Landing2 from "./pages/Landing2";
-import HomePage from "./pages/Homepage";
-import TMSLandingPage from "./pages/Component";
-import RenaissanceVisitSection from "./pages/Comp2";
-import Comp3 from "./pages/Comp3";
-import Landing4 from "./pages/Landing3";
 
 const router = createBrowserRouter([
   {
@@ -15,26 +10,6 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Landing2 />,
-      },
-      {
-        path: "/home",
-        element: <HomePage />,
-      },
-      {
-        path: "/comp",
-        element: <TMSLandingPage />,
-      },
-      {
-        path: "/comp2",
-        element: <RenaissanceVisitSection />,
-      },
-      {
-        path: "/comp3",
-        element: <Comp3 />,
-      },
-      {
-        path: "/comp4",
-        element: <Landing4 />,
       },
     ],
   },
